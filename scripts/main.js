474887d58f6fe1f282121627f9dfd7d0587e1268
Michael David <mdavid01@gmail.com>
2023-11-18 07:11:32 -0700

    trying to expand name column

--- scripts/main.js
+++ scripts/main.js
@@ -56,7 +56,7 @@ function loadTable(valueArray, initialLoad){
             getResourceTypes(element);
         }
 
-        nameCell.innerHTML = "<a href='display.html?entry="+ element.id +"' target='_blank'>" +element.name+ "</a>";
+        nameCell.innerHTML = "<span style="width:50%"><a href='display.html?entry="+ element.id +"' target='_blank'>" +element.name+ "</a></span>";
         descriptionCell.innerHTML = element.description;
     }
 
