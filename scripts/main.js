157e3cdd5d49d9c23418b57dbf188ec2028608af
Michael David <mdavid01@gmail.com>
2023-11-18 07:03:20 -0700

    removing ; because it wasn't the issue

--- scripts/main.js
+++ scripts/main.js
@@ -56,7 +56,7 @@ function loadTable(valueArray, initialLoad){
             getResourceTypes(element);
         }
 
-        nameCell.innerHTML = "<a href='display.html?entry="+ element.id +"' target='_blank'>" +element.name+ "</a>";;
+        nameCell.innerHTML = "<a href='display.html?entry="+ element.id +"' target='_blank'>" +element.name+ "</a>";
         descriptionCell.innerHTML = element.description;
     }
 
