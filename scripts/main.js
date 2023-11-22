a8cf372761cad543bf99c684935ce500d0eca8a0
Shakil hossain <developershakil2@gmail.com>
2023-11-22 20:05:42 +0600

    Update main.js
    updated alphabet sorted and phone clickable

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,17 @@
-
 var pageTitle = "Missoula Resources";
-var pageDescription = "We care about you!"
-
-
-
+var pageDescription = "We care about you!";
 var resourceTypes = ['anything'];
 
-function onPageLoad(){
-    
+function onPageLoad() {
     var old_tbody = document.getElementById("resourceTableBody");
     var newTableBody = loadTable(entries, true);
     old_tbody.parentNode.replaceChild(newTableBody, old_tbody);
-    
+
     var select = document.getElementById("resourceSelect");
-    
-    for(let i=0; i < resourceTypes.length; i++){
+
+    resourceTypes.sort(); // Sort resourceTypes alphabetically
+
+    for (let i = 0; i < resourceTypes.length; i++) {
         var opt = document.createElement("option");
         opt.value = resourceTypes[i].toLowerCase();
         opt.innerHTML = resourceTypes[i].toUpperCase();
@@ -24,62 +21,72 @@ function onPageLoad(){
     setPageDetails();
 }
 
-function setPageDetails(){
-    var title = document.getElementById("pageTitle");
-    title.textContent = pageTitle;
-
-    var description = document.getElementById("pageDescription");
-    description.textContent = pageDescription;
-}
-
-function getResourceTypes(element){
-    for(let j=0; j < element.keywords.length; j++){
+function getResourceTypes(element) {
+    for (let j = 0; j < element.keywords.length; j++) {
         var resource = element.keywords[j];
-        if(!resourceTypes.includes(resource)){
+        if (!resourceTypes.includes(resource)) {
             resourceTypes.push(resource);
         }
     }
-    resourceTypes.sort();
 }
 
-function loadTable(valueArray, initialLoad){
+// Sort resourceTypes alphabetically before using it
+resourceTypes.sort();
+
+function loadTable(valueArray, initialLoad) {
+    // Sort valueArray alphabetically by name
+    valueArray.sort((a, b) => a.name.localeCompare(b.name));
+
     var new_tbody = document.createElement('tbody');
     new_tbody.setAttribute("id", "resourceTableBody");
-    for( let index=0; index < valueArray.length; index++){
+
+    for (let index = 0; index < valueArray.length; index++) {
         var element = valueArray[index];
         var row = new_tbody.insertRow(index);
-        row.setAttribute("class", index%2==0?"evenRow":"oddRow");
+        row.setAttribute("class", index % 2 == 0 ? "evenRow" : "oddRow");
         var nameCell = row.insertCell(0);
         var descriptionCell = row.insertCell(1);
+        var phoneCell = row.insertCell(2); // Added cell for phone number
 
-        if(initialLoad){
+        if (initialLoad) {
             getResourceTypes(element);
         }
 
-        nameCell.innerHTML = "<h3><a href='display.html?entry="+ element.id +"' target='_blank'>" +element.name+ "</a></h3>";
+        nameCell.innerHTML = "<h3><a href='display.html?entry=" + element.id + "' target='_blank'>" + element.name + "</a></h3>";
         descriptionCell.innerHTML = element.description;
+
+        // Add a phone link to the row
+      
     }
 
     return new_tbody;
 }
 
-function updateGrid(){
+function createPhoneLink(phoneNumber) {
+    var phoneLink = document.createElement('a');
+    phoneLink.href = "tel:" + phoneNumber;
+    phoneLink.textContent = phoneNumber;
+    return phoneLink;
+}
+
+function updateGrid() {
     var select = document.getElementById("resourceSelect");
     var value = select.value;
     var filteredOptions = [];
-    if(value == 'anything'){
+
+    if (value == 'anything') {
         filteredOptions = entries;
     }
 
-    for(let i=0; i<entries.length; i++){
+    for (let i = 0; i < entries.length; i++) {
         var entry = entries[i];
-        if(entry.keywords.includes(value)){
+        if (entry.keywords.includes(value)) {
             filteredOptions.push(entry);
         }
     }
 
-     var newTableBody = loadTable(filteredOptions, false);
-     var old_tbody = document.getElementById("resourceTableBody");
+    var newTableBody = loadTable(filteredOptions, false);
+    var old_tbody = document.getElementById("resourceTableBody");
 
-     old_tbody.parentNode.replaceChild(newTableBody, old_tbody);
+    old_tbody.parentNode.replaceChild(newTableBody, old_tbody);
 }
